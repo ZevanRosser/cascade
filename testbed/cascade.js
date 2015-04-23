3617c057833ee645dfba7c22bd59edcf0bc96acc
ZevanRosser <bendvent@gmail.com>
2015-04-23 14:42:32 -0400

    lots of updates

--- testbed/cascade.js
+++ testbed/cascade.js
@@ -6,42 +6,48 @@
     });
   };
   
-  var mainAliases = [ 'm', 'method', 'cascade', 'run', 'set', '$', '_' ],
+  var mainAliases = [ 'm', 'method', 'cascade', 'run', 'set', '$', 'f'],
       returnValueAliases = [ 'r', 'ret', 'dive' ],
       // .r(setable, method), .r('method')
       // .r() (index - 1)
       // .top() = .r(values.length-1)
-      leapAliases = [ 'l', 'up', 'leap', 'prev' ];
+      leapAliases = [ 'l', 'up', 'leap', 'prev' ],
+      nextAliases = [ 'n', 'next', '_' ];
+  
+  // next aliases
+  
+  var aliases = function(target, names, func) {
+    names.forEach(function(name) {
+     target[name] = func;
+    });
+    return aliases.bind(null, target);
+  };
   
   var Cascader = function(obj, method) {
     var ret, leap;
     
+    
+    
     this.values = [];
     this.objects = [obj];
+    this.state = {};
     this.obj = obj;
     this.method = method;
     this.cascade = cascade.bind(this, obj);
     this.boundRun = this.run.bind(this);
+    this.boundRun.s = this.setState.bind(this);
+    this.boundRun.state = this.state;
     
-    mainAliases.forEach(function(val) {
-      this.boundRun[val] = this.cascade;
-    }.bind(this));
+    aliases(this.boundRun, mainAliases, this.cascade)
+           (returnValueAliases, this.ret.bind(this))
+           (leapAliases, this.leap.bind(this))
+           (nextAliases, this.next.bind(this));
     
     this.boundRun.first = this.first.bind(this);
     this.boundRun.last = this.last.bind(this);
     this.boundRun.vals = this.vals.bind(this);
     
-    ret = this.ret.bind(this);
-    returnValueAliases.forEach(function(val) {
-      this.boundRun[val] = ret;
-    }.bind(this));
-    
-    leap = this.leap.bind(this);
-    leapAliases.forEach(function(val) {
-      this.boundRun[val] = leap;
-    }.bind(this));
-    
-    this.boundRun.next = this.next.bind(this);
+    this.boundRun.i = this.i.bind(this);
     
     this.boundRun.all = {
       first: this.allFirst.bind(this),
@@ -55,10 +61,46 @@
     
     constructor: Cascader,
     
+    setState: function(keyOrObj, value) {
+      if (typeof keyOrObj === 'string') {
+        this.boundRun.s[keyOrObj] = value;
+      } else {
+        setProps(this.boundRun.s, keyOrObj);  
+      }
+      return this.boundRun;
+    },
+    
     set: function(props, method) {
-      setProps(this.obj, props);
-      console.log('set', arguments);
-      if (method) { this.method = method; console.log('method')}
+      setProps(this.obj, props); 
+      if (method) { this.method = method; }
+      return this.boundRun;
+    },
+    
+    i: function(start, end, stepOrFunc, func) {
+      var hasStep = !isNaN(parseFloat(stepOrFunc)), 
+          step, loopFunc, i;
+      if (!hasStep) {
+        step = (start < end) ? 1 : -1;
+        loopFunc = stepOrFunc;
+      } else {
+        step = stepOrFunc;
+        loopFunc = func;
+      }
+      
+      if (step > 0) {
+        i = start;
+        while(i <= end) {
+          loopFunc(this.boundRun, i);
+          i += step; 
+        }
+      } else {
+        i = end;
+        while(i >= start) {
+          loopFunc(this.boundRun, i);
+          i += step; 
+        }
+      }
+      
       return this.boundRun;
     },
     
@@ -129,18 +171,25 @@
     
   };
   
+  var current;
+  
   window.cascade = function(obj, method, otherMethod) {
     var cascader;
+    
+    if (arguments.length === 0 && current) { 
+      return current.boundRun;
+    }
     
     if (this instanceof Cascader) {
       cascader = this;
       
-      if (!method) {
-        console.log(obj);
-        return cascade.bind(cascader, obj) ;
+      if (method == null) {
+        return cascade.bind(cascader, obj);
       }
       
-      if (!(typeof method === 'string')) {
+      if (typeof method === 'number') {
+        cascader.i.apply(cascader, Array.prototype.slice.call(arguments, 1));
+      } else if (!(typeof method === 'string')) {
         cascader.set(method, otherMethod);
       } else {
         cascader.method = method;
@@ -149,6 +198,8 @@
     } else {
       cascader = new Cascader(obj, method);
     }
+    
+    current = cascader;
     
     return cascader.boundRun;
   };
